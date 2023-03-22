9e92661a795e984d6c383d6b4192a3d7f75f6fc4
takatoshiyamada <takatoshiyamada.3505@gmail.com>
2023-03-22 20:01:13 +0900

    Update: day2

--- day2.rb
+++ day2.rb
@@ -1,26 +1,33 @@
 def selection_sort(arr)
   n = arr.length
 
-  # 配列の要素を順番に比較し、最小値のインデックスを探す
+  # Compare array elements
+  # in order and find the index
+  # with the smallest value
   (0..n-2).each do |i|
     min_idx = i
-    (i+1..n-1).each do |j|
+    (i + 1..n-1).each do |j|
       min_idx = j if arr[j] < arr[min_idx]
     end
 
-    # 最小値を先頭要素と交換する
+    # Exchange the minimum value
+    # with the first element.
     arr[i], arr[min_idx] = arr[min_idx], arr[i]
   end
 
-  return arr
+  arr
+
 end
 
-# テスト用の配列
-arr = [64, 25, 12, 22, 11]
-puts arr.to_s
+arr = [64, 25, 33, 12, 444]
 
-# 選択ソートを実行する
+# different p and puts
+p arr.to_s
+puts arr.to_s
+p arr.to_s.class
+puts arr.to_s.class
+
 sorted_arr = selection_sort(arr)
 
-# 結果を表示する
-puts sorted_arr.to_s  # => [11, 12, 22, 25, 64]
+# Result
+puts sorted_arr.to_s

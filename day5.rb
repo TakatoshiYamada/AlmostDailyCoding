061eb22232324e4274f903675f3f030de537039c
takatoshiyamada <takatoshiyamada.3505@gmail.com>
2023-03-26 19:05:27 +0900

    Update: day5

--- day5.rb
+++ day5.rb
@@ -1,13 +1,26 @@
+# いくつかの数字が入ったリストが与えられたとき、そのリスト内の全ての偶数の平均値を求めるプログラムを作成してください。
+# 例えば、与えられたリストが [1, 2, 3, 4, 5] の場合、偶数の平均値は (2 + 4) / 2 = 3 となります。
+# また、リストが空の場合や偶数が含まれていない場合は、0を返すようにしてください。
+# これを解決するためのプログラムを書いてみてください。
+
 args = ARGV
 
 result = 0
+even_cnt = 0
 if args.length > 0
   args.each do |arg|
-    result + arg if arg % 2 == 0
+    if arg.to_i % 2 == 0
+      result += arg.to_i
+      even_cnt += 1
+    end
   end
 
-  avg = result / result.length
-  puts "avg = ${result/resu}"
+  if even_cnt > 0
+    even_avg = result / even_cnt
+    puts "even_avg = #{even_avg}"
+  else
+    puts "no even number in argv"
+  end
 else
-  puts 'no args'
+  puts "no argv."
 end

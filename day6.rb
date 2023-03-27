697322a7d7000fcb336791aaf8edb9e1d92e9080
takatoshiyamada <takatoshiyamada.3505@gmail.com>
2023-03-27 21:04:58 +0900

    Add: Day6

---
+++ day6.rb
@@ -0,0 +1,14 @@
+fruits = ['apple', 'banana', 'orange']
+
+puts fruits[0]
+puts fruits[1]
+puts fruits[2]
+
+fruits[1] = 'kiwi'
+
+puts fruits.inspect
+
+fruits << 'grape'
+fruits.push('wataermelon')
+
+puts fruits.inspect

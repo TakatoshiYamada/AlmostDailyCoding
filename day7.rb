786c5c35cbe64f2f23e936529703435e87fb4ef5
takatoshiyamada <takatoshiyamada.3505@gmail.com>
2023-03-28 21:04:43 +0900

    Add: day7

---
+++ day7.rb
@@ -0,0 +1,10 @@
+fruits = ['apple', 'banana', 'grape']
+
+fruits.push('elderberry')
+puts fruits
+puts fruits.pop
+puts fruits
+puts fruits.unshift('apricot')
+puts fruits
+puts fruits.shift
+puts fruits

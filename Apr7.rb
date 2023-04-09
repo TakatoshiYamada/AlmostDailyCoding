1d7b380b38f5cf075f76ff9a1c8c036e350521d4
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-04-09 22:53:29 +0900

    Update: ヒープソートを更にリファクタリング

--- Apr7.rb
+++ Apr7.rb
@@ -1,36 +1,39 @@
 def heap_sort(arr)
   n = arr.length
+  last_index = n - 1
 
   # ヒープ構造の作成
-  (n - 1).downto(0) do | i |
+  last_index.downto(0) do |i|
     heapify(arr, n, i)
   end
 
   # ヒープ構造から最大値を取り出して、ソート済みデータに追加する
-  (n - 1).downto(1) do |i|
+  last_index.downto(1) do |i|
     arr[0], arr[i] = arr[i], arr[0]
     heapify(arr, i, 0)
   end
   arr
 end
 
-def heapify(arr, length, index)
+def update_largest(arr, length, index)
   largest = index
   left = 2 * index + 1
   right = 2 * index + 2
 
-  if left < length && arr[left] > arr[largest]
-    largest = left
-  end
+  largest = left if left < length && arr[left] > arr[largest]
 
-  if right < length && arr[right] > arr[largest]
-    largest = right
-  end
+  largest = right if right < length && arr[right] > arr[largest]
 
-  if largest != index
-     arr[index], arr[largest] = arr[largest], arr[index]
-    heapify(arr, length, largest)
-  end
+  largest
+end
+
+def heapify(arr, length, index)
+  largest = update_largest(arr, length, index)
+
+  return unless largest != index
+
+  arr[index], arr[largest] = arr[largest], arr[index]
+  heapify(arr, length, largest)
 end
 
 args = ARGV.map(&:to_i)

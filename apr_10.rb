bbc3b1f46cf2ed129ffd5542895dd440b865f482
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-04-10 20:28:48 +0900

    Add: アルゴ式 標準出力 ~1-5

---
+++ apr_10.rb
@@ -0,0 +1,16 @@
+result = 27_182 % 818
+
+puts result.to_s
+
+result2 = 314 * (159 + 265) - 358
+
+puts result2.to_s
+
+# 1 日は 24 時間、 1 時間は 60 分、1 分は 60 秒です。
+# 1 日は何秒かを計算し、整数で出力するプログラムを作成してください。
+
+min2sec = 60
+hour2sec = 60 * min2sec
+day2sec = 24 * hour2sec
+
+puts day2sec

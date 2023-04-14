b55c8bf6ed533bcfd0466aa504e2da28f4d9e38d
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-04-14 21:47:44 +0900

    Update: mergeメソッド内の制御を三項演算子に変更

--- apr_14.rb
+++ apr_14.rb
@@ -23,13 +23,9 @@ def merge(left, right)
   # 左右の配列のどちらかが空になるまで繰り返す
   until left.empty? || right.empty?
     # 左右の配列の先頭を比較し、小さい方を結果に追加する
-    sorted << if left.first <= right.first
-                # 左の配列の先頭を削除し、結果に追加する
-                left.shift
-              else
-                # 右の配列の先頭を削除し、結果に追加する
-                right.shift
-              end
+    # 左の配列の先頭を削除し、結果に追加する
+    # 右の配列の先頭を削除し、結果に追加する
+    sorted << (left.first <= right.first) ? left.shift : right.shift
   end
 
   # 左右の配列のどちらかに要素が残っている場合は、結果に追加する

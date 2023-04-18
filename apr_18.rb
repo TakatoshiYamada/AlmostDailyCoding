85c09280487edef6d9188b7d62cfb7a0b93edef2
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-04-18 23:17:43 +0900

    Update: 無駄な空白削除

--- apr_18.rb
+++ apr_18.rb
@@ -2,7 +2,7 @@
 # Write a method linear_search(arr, target) that accepts an array and another input as target. The method should use a linear search to return the position of the element where the target is found. If the target is not found, return -1. You can assume the array contains unique elements.
 # Examples
 # linear_search(["a", "b", "c"], "b") # => 1
-# linear_search(["a", "b", "c"], "f") # => -1 
+# linear_search(["a", "b", "c"], "f") # => -1
 # linear_search([42, 89, 23, 1], 1) # => 3
 def linear_search(arr, target)
   arr.each.with_index do |el, i|
@@ -28,6 +28,6 @@ puts "The index of #{target} is #{result}"
 
 # ❯ ruby apr_18.rb
 # 1 2 3
-# Enter a number to search for: 
+# Enter a number to search for:
 # 1
 # The index of 1 is 0
